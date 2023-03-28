7fef1eda9dea0d0b968af1c2868a4919c6bf3acd
Victoria D <116195324+vdynak@users.noreply.github.com>
2023-03-28 16:51:03 -0400

    Add files via upload
    File upload

---
+++ Models/User.swift
@@ -0,0 +1,25 @@
+// Victoria Dynak
+// BeReal
+// Created on March 28, 2023
+
+import Foundation
+import ParseSwift
+
+struct User: ParseUser {
+    // These are required by `ParseObject`.
+    var objectId: String?
+    var createdAt: Date?
+    var updatedAt: Date?
+    var ACL: ParseACL?
+    var originalData: Data?
+
+    // These are required by `ParseUser`.
+    var username: String?
+    var email: String?
+    var emailVerified: Bool?
+    var password: String?
+    var authData: [String: [String: String]?]?
+
+    // Your custom properties.
+    // var customKey: String?
+}
